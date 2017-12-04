6d8c5df72faa0f1c10781aea88e5947d7e0502bc
Navid Shakibapour <navid.shakibapour@gmail.com>
2017-12-04 13:57:53 -0500

    Added YAML-to-JSON converter servlet + Added a sample test
    Signed-off-by: Navid Shakibapour <navid.shakibapour@gmail.com>

---
+++ tck/src/main/java/org/eclipse/microprofile/openapi/tck/SampleTest.java
@@ -0,0 +1,112 @@
+/**
+ * Copyright (c) 2017 Contributors to the Eclipse Foundation
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.eclipse.microprofile.openapi.tck;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.equalTo;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.eclipse.microprofile.openapi.tck.utils.YamlToJsonConverterServlet;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.container.test.api.RunAsClient;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import io.restassured.RestAssured;
+import io.restassured.parsing.Parser;
+import io.restassured.response.Response;
+
+@RunWith(Arquillian.class)
+public class SampleTest {
+    private static final String APPLICATION_JSON = "application/json";
+    private static final String APPLICATION_YAML = "application/yaml";
+
+    private static final String DEFAULT_PROTOCOL = "http";
+    private static final String DEFAULT_HOST = "localhost";
+    private static final int DEFAULT_PORT = 9080;
+
+    @BeforeClass
+    static public void setup() throws MalformedURLException {
+        // set base URI and port number to use for all requests
+        String serverUrl = System.getProperty("test.url");
+        String protocol = DEFAULT_PROTOCOL;
+        String host = DEFAULT_HOST;
+        int port = DEFAULT_PORT;
+
+        if (serverUrl != null) {
+            URL url = new URL(serverUrl);
+            protocol = url.getProtocol();
+            host = url.getHost();
+            port = (url.getPort() == -1) ? DEFAULT_PORT : url.getPort();
+        }
+
+        RestAssured.baseURI = protocol + "://" + host;
+        RestAssured.port = port;
+
+        String userName = System.getProperty("test.user");
+        String password = System.getProperty("test.pwd");
+
+        if (userName != null && password != null) {
+            RestAssured.authentication = RestAssured.basic(userName, password);
+            RestAssured.useRelaxedHTTPSValidation();
+        }
+        System.out.println("TEST:"+userName + ":" + password + "@" + serverUrl);
+    }
+
+    @Deployment(name = "proxy")
+    public static WebArchive createProxy() {
+        WebArchive war = ShrinkWrap.create(WebArchive.class, "proxy.war")
+                .addClass(YamlToJsonConverterServlet.class)
+                .addAsLibraries(new File("./lib/httpclient-4.5.2.jar"))
+                .addAsLibraries(new File("./lib/httpcore-4.4.4.jar"))
+                .addAsLibraries(new File("./lib/jackson-core-2.9.2.jar"))
+                .addAsLibraries(new File("./lib/jackson-dataformat-yaml-2.9.2.jar"))
+                .addAsLibraries(new File("./lib/jackson-databind-2.9.2.jar"))
+                .addAsLibraries(new File("./lib/jackson-annotations-2.9.1.jar"))
+                .addAsLibraries(new File("./lib/snakeyaml-1.18.jar"))
+                .addAsLibraries(new File("./lib/commons-logging-1.2.jar"));
+        System.out.println(war.toString(true));
+        return war;
+    }
+
+    @Deployment(name = "airlines")
+    public static WebArchive createDeployment() {
+        WebArchive war = ShrinkWrap.create(WebArchive.class, "airlines.war")
+                .addPackages(true, "org.eclipse.microprofile.openapi.apps.airlines")
+                .addAsManifestResource(EmptyAsset.INSTANCE, "/openapi/openapi.yaml");
+        System.out.println(war.toString(true));
+        return war;
+    }
+
+    @Test
+    @RunAsClient
+    public void testTextPlainResponseContentType() throws InterruptedException {
+        Response response = given().when().get("/proxy");
+        response.then().parser("", Parser.JSON).statusCode(200).body("openapi", equalTo("3.0.0")).body("info.description", containsString("Liberty"));
+        String doc = response.asString();
+        System.out.println(doc);
+    }
+}
---
+++ tck/src/main/java/org/eclipse/microprofile/openapi/tck/utils/YamlToJsonConverterServlet.java
@@ -0,0 +1,137 @@
+/**
+ * Copyright (c) 2017 Contributors to the Eclipse Foundation
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.eclipse.microprofile.openapi.tck.utils;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.Enumeration;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+
+@WebServlet("/")
+public class YamlToJsonConverterServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    private static final String APPLICATION_JSON = "application/json";
+    private static final String APPLICATION_YAML = "application/yaml";
+
+    private static final String DEFAULT_PROTOCOL = "http";
+    private static final String DEFAULT_HOST = "localhost";
+    private static final int DEFAULT_PORT = 9080;
+
+    private static final String OPENAPI_PATH = "/api/docs";
+
+    private String username;
+    private String password;
+    private String serverUrl;
+
+    @Override
+    public void init() throws ServletException {
+        username = System.getProperty("test.user");
+        password = System.getProperty("test.pwd");
+
+        serverUrl = System.getProperty("test.url");
+        if (serverUrl == null) {
+            serverUrl = DEFAULT_PROTOCOL + "://" + DEFAULT_HOST + ":" + DEFAULT_PORT;
+        }
+    }
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        BufferedReader rd = null;
+        PrintWriter writer = null;
+
+        try {
+            HttpClientBuilder builder = HttpClientBuilder.create();
+            if (username != null && password != null) {
+                CredentialsProvider provider = new BasicCredentialsProvider();
+                UsernamePasswordCredentials credentials = new UsernamePasswordCredentials(username, password);
+                provider.setCredentials(AuthScope.ANY, credentials);
+                builder.setDefaultCredentialsProvider(provider);
+            }
+
+            HttpClient client = builder.build();
+            HttpGet targetRequest = new HttpGet(serverUrl + OPENAPI_PATH);
+
+            // Copy request headers
+            Enumeration<String> headerNames = request.getHeaderNames();
+            while (headerNames.hasMoreElements()) {
+                String headerName = headerNames.nextElement();
+                Enumeration<String> headers = request.getHeaders(headerName);
+                while (headers.hasMoreElements()) {
+                    targetRequest.addHeader(headerName, headers.nextElement());
+                }
+            }
+
+            // Forward the request
+            HttpResponse targetResponse = client.execute(targetRequest);
+
+            // Copy response headers
+            response.setStatus(targetResponse.getStatusLine().getStatusCode());
+            Arrays.stream(targetResponse.getAllHeaders()).forEach(h -> response.addHeader(h.getName(), h.getValue()));
+
+            // Copy the content
+            rd = new BufferedReader(new InputStreamReader(targetResponse.getEntity().getContent()));
+            String line = "";
+            StringBuffer sb = new StringBuffer();
+            while ((line = rd.readLine()) != null) {
+                sb.append(line).append("\n");
+            }
+
+            String yaml = sb.toString();
+            ObjectMapper yamlReader = new ObjectMapper(new YAMLFactory());
+            Object obj = yamlReader.readValue(yaml, Object.class);
+
+            ObjectMapper jsonWriter = new ObjectMapper();
+            String json = jsonWriter.writeValueAsString(obj);
+
+            writer = response.getWriter();
+            writer.print(json);
+            writer.close();
+        }
+        catch (Exception e) {
+            response.sendError(500, "Failed to convert the request: " + e);
+        }
+        finally {
+            if (rd != null) {
+                rd.close();
+            }
+            if (writer != null) {
+                writer.close();
+            }
+        }
+    }
+}
