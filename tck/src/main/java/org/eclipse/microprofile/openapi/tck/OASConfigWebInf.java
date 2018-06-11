78fd9938172169338c48be11bb3548d1c2c15df8
Matt Gill <matthew.gill@live.co.uk>
2018-06-11 10:27:46 +0100

    Added a WEB-INF resource copy of the OASConfigScanClassTest
    Signed-off-by: Matt Gill <matthew.gill@live.co.uk>

---
+++ tck/src/main/java/org/eclipse/microprofile/openapi/tck/OASConfigWebInf.java
@@ -0,0 +1,32 @@
+/**
+ * Copyright (c) 2018 Contributors to the Eclipse Foundation
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.eclipse.microprofile.openapi.tck;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+
+public class OASConfigWebInf extends OASConfigScanClassTest {
+
+    @Deployment(name = "airlinesWebInf", order = 2)
+    public static WebArchive createDeployment() {
+        return ShrinkWrap.create(WebArchive.class, "airlinesWebInf.war")
+                .addPackages(true, "org.eclipse.microprofile.openapi.apps.airlines")
+                .addAsWebInfResource("class-microprofile-config.properties", "classes/META-INF/microprofile-config.properties");
+    }
+
+}
