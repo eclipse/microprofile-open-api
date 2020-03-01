54822cd7014a4c01a781b7f281527f9c85d028c3
Michael Edgar <michael@xlate.io>
2020-03-01 05:52:14 -0500

    Update APIResponseSchema documentation to describe default behavior
    Signed-off-by: Michael Edgar <michael@xlate.io>

--- api/src/main/java/org/eclipse/microprofile/openapi/annotations/responses/APIResponseSchema.java
+++ api/src/main/java/org/eclipse/microprofile/openapi/annotations/responses/APIResponseSchema.java
@@ -45,15 +45,9 @@ import java.lang.annotation.Target;
  * annotation will result in those media types applying to the OpenAPI response model.
  *
  * <p>
- * The default response code shall be determined using REST conventions as follows:
- * <ul>
- * <li>If the method's return type is <code>void</code> and the HTTP method is <code>&#64;POST</code>,
- *     the status code will be <code>201</code> and the description will be "Created"
- * <li>Otherwise, if the method's return type is <code>void</code> the method does not list a JAX-RS
- *     <code>AsyncResponse</code> parameter, the status code will be <code>204</code> and the description
- *     will be "No Content".
- * <li>Otherwise, the status code will be <code>200</code> and the description will be "OK".
- * </ul>
+ * If not specified, default responseCode and responseDescription values shall be determined
+ * according to the {@link #responseCode() responseCode} and {@link #responseDescription() responseDescription}
+ * documentation.
  *
  * <pre>
  * &#64;GET
@@ -65,6 +59,7 @@ import java.lang.annotation.Target;
  * }
  * </pre>
  *
+ * @since 2.0
  * @see APIResponse
  * @see "https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.0.md#responseObject"
  *
@@ -84,16 +79,32 @@ public @interface APIResponseSchema {
     Class<?> value();
 
     /**
-     * A short description of the response. It is a REQUIRED property.
+     * A short description of the response. It is a REQUIRED property in the OpenAPI document.
+     *
+     * <p>
+     * If no value is specified, the default value will set to the description given by the
+     * <a href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html#sec10">HTTP/1.1 documentation</a>
+     * for the {@link #responseCode() responseCode} in use.
      * 
      * @return description of the response.
      **/
-    String responseDescription();
+    String responseDescription() default "";
 
     /**
-     * The HTTP response code, or 'default', for the supplied response. May only have 1 default entry.
-     * 
+     * The HTTP response code, or 'default', for the supplied response. May have only one 'default' entry.
+     *
+     * <p>
+     * If no value is specified, a default shall be determined using REST conventions as follows:
+     *
+     * <ul>
+     * <li>If the method's return type is <code>void</code> and the HTTP method is <code>&#64;POST</code>,
+     *     the code will be <code>201</code>.
+     * <li>Otherwise, if the method's return type is <code>void</code> the method does not list a JAX-RS
+     *     <code>AsyncResponse</code> parameter, the code will be <code>204</code>.
+     * <li>Otherwise, the code will be <code>200</code>.
+     * </ul>
+     *
      * @return HTTP response code for this response instance or default
      **/
-    String responseCode() default "default";
+    String responseCode() default "";
 }
