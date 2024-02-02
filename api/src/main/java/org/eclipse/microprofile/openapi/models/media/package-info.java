752ce591fd377e4717e519c290e34c9dd21583ad
Andrew Rouse <anrouse@uk.ibm.com>
2024-02-02 16:15:31 +0000

    Update package version for breaking model changes

--- api/src/main/java/org/eclipse/microprofile/openapi/models/media/package-info.java
+++ api/src/main/java/org/eclipse/microprofile/openapi/models/media/package-info.java
@@ -30,6 +30,6 @@
  * </pre>
  */
 
-@org.osgi.annotation.versioning.Version("2.2")
+@org.osgi.annotation.versioning.Version("3.0")
 @org.osgi.annotation.versioning.ProviderType
 package org.eclipse.microprofile.openapi.models.media;
